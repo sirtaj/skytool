5ec6ada14102f0ceaf7460caed78e9bb4aaa6cea
Sirtaj Singh Kang <sirtaj@sirtaj.net>
2013-02-19 14:25:35 +0530

    Thread helper.

--- skytool/qutil.py
+++ skytool/qutil.py
@@ -1,4 +1,7 @@
-from PyQt4.QtCore import QObject, SIGNAL
+
+import PyQt4.QtCore as qc
+import PyQt4.QtGui as qg
+from PyQt4.QtCore import SIGNAL
 
 def qmicro(iterations=500):
      '''Qt fire-and-forget microprocess decorator.
@@ -17,12 +20,12 @@ def qmicro(iterations=500):
      return wrap_qmicro
 
 
-class QtMicroProcess(QObject):
+class QtMicroProcess(qc.QObject):
      '''A single running microprocess, scheduled in the event loop using
      timer events until completed or error.
      '''
      def __init__(self, parent, next_fn, iterations):
-         QObject.__init__(self, parent)
+         qc.QObject.__init__(self, parent)
          self.next_fn = next_fn
          self.iterations = iterations
          self.timer_id = self.startTimer(0)
@@ -57,3 +60,92 @@ def print_time(fn):
     return run_it
 
 
+
+class MyThread(qc.QThread):
+    '''Basic fire and forget thread that calls thread_fn and calls on_finish when done.
+
+        thread_fn takes no arguments and returns whatever.
+        on_finish takes one argument, which is the returned value of thread_fn
+        on_error takes one argument, which is the raised exception.
+    '''
+
+    def __init__(self, thread_fn, on_finish = None, on_error = None, parent = None):
+        super(MyThread, self).__init__(parent)
+        self.thread_fn = thread_fn
+        self.on_finish = self.null_finish if on_finish is None else on_finish
+        self.on_error = self.display_error if on_error is None else on_error
+
+        self.exception = None
+        self.return_value = None
+        self.one_shot = False
+
+        self.finished.connect( self.handle_finished )
+        self.terminated.connect( self.handle_finished )
+
+    def null_finish(self): pass
+
+    def handle_finished(self):
+        try:
+            if self.exception is not None:
+                self.on_error(self.exception)
+            else:
+                self.on_finish(self.return_value)
+        except BaseException, e:
+            self.display_error(e)
+        finally:
+            self.exception = None
+            self.return_value = None
+            if self.one_shot:
+                self.deleteLater()
+
+    def run(self):
+        try:
+            self.return_value = self.thread_fn()
+        except BaseException, e:
+            self.exception = e
+        finally:
+            return
+
+    def display_error(self, exc):
+        qc.QMessageBox.critical(
+                parent = self,
+                title = "Unhandled Error: %s" % (exc.__class__.__name__),
+                text = str(exc) )
+
+    @classmethod
+    def one_shot(Cls, thread_fn, on_finish = None, on_error = None, parent = None):
+        thread = Cls(thread_fn, on_finish, on_error, parent)
+        thread.one_shot = True
+        thread.start()
+        return thread
+
+    @classmethod
+    def process(Cls, process_fn, on_error = None):
+        '''A decorator to mark a fire and forget threaded process.
+        '''
+        def wrapped_process(*args, **kwargs):
+            def run_it():
+                process_fn(*args, **kwargs)
+
+            Cls.one_shot(thread_fn = run_it, on_finish = None, on_error = None, parent = None)
+
+        return wrapped_process
+
+def test_thread():
+    import sys
+    def process():
+        for a in xrange(20): print a
+        print "done"
+        return 42
+
+    def on_done(v):
+        print "returned with:", v
+
+    app = qg.QApplication(sys.argv[1:])
+    MyThread.one_shot(process, on_finish = on_done)
+    app.exec_()
+    import time
+    time.sleep(5)
+
+if __name__ == '__main__':
+    test_thread()
